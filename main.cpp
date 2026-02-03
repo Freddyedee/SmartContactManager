770339278cb05568f14191de50645534237ac8fd
wrmonki <sebasargotte@gmail.com>
2026-02-03 19:44:59 -0400

    Merge branch 'RamaF' into RamaS

--- main.cpp
+++ main.cpp
@@ -37,11 +37,13 @@ int main(int argc, char *argv[])
     Garantiza que la salida ocurra después de que Qt termine de procesar la señal.
     */
     engine.loadFromModule("AgendaContactosInteligente", "Main");
-//sssss
     return app.exec();
 }
 
 //COMENTARIO PRUEBA MONKI WR
 int suma(int a, int b, int c){
     return a + b + c;
+}
+int resta(int a, int b){
+    return a - b;
 }
